5a54807964b31c78f662b17b99e13fb334f16027
Michael Yang <themichaelyang@gmail.com>
2020-08-26 16:06:46 -0400

    add Iter.map test

--- test/iterable_test.exs
+++ test/iterable_test.exs
@@ -6,4 +6,10 @@ defmodule IterableTest do
         assert [] |> Iter.reduce(0, sum_up) == 0
         assert [1, 2, 3, 4] |> Iter.reduce(0, sum_up) == 10
     end
+
+    test 'list is mappable' do
+        identity = &(&1)
+        assert [1, 2, 3, 4] |> Iter.map(identity) == [1, 2, 3, 4]
+        assert [1, 2, 3, 4] |> Iter.map(&(&1 * 2)) == [2, 4, 6, 8] 
+    end
 end
